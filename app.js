91659b5ad6d9de17fd5da24055f4feb0f80d48c3
Saksham Singh <geekysaksham27@gmail.com>
2019-11-16 22:03:58 +0530

    calculation part done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// BUDGET controller ******************************************************************
+// BUDGET controller ************************************************************************
+// ******************************************************************************************
 var budgetController = (function() {
+	// ----------------
 	var Expense = function(id, description, value) {
 		this.id = id;
 		this.description = description;
@@ -11,7 +13,19 @@ var budgetController = (function() {
 		this.description = description;
 		this.value = value;
 	};
+	// ----------------
 
+	// ----------------
+	var calculateTotal = function(type) {
+		var sum = 0;
+		data.allItems[type].forEach(function(curr) {
+			sum += curr.value;
+		});
+		data.totals[type] = sum;
+	};
+	// ----------------
+
+	// ----------------
 	var data = {
 		allItems : {
 			expense : [],
@@ -21,11 +35,15 @@ var budgetController = (function() {
 		totals   : {
 			expense : 0,
 			income  : 0
-		}
+		},
+
+		budget   : 0
 	};
+	// ----------------
 
 	return {
-		addItem : function(type, des, val) {
+		// ----------------
+		addItem         : function(type, des, val) {
 			var newItem, ID;
 
 			// create new ID
@@ -48,29 +66,62 @@ var budgetController = (function() {
 			// return the new element
 			return newItem;
 		},
+		// ----------------
 
-		testing : function() {
+		// ----------------
+		calculateBudget : function() {
+			// 1. Calculate total income and expenses
+			calculateTotal('income');
+			calculateTotal('expense');
+
+			// 2. Calculate the budget : income -budget
+			data.budget = data.totals.income - data.totals.expense;
+
+			// 3. Calculate the percentageof income that we spent
+		},
+		// ----------------
+
+		// ----------------
+		getBudget       : function() {
+			return {
+				budget       : data.budget,
+				totalIncome  : data.totals.income,
+				totalExpense : data.totals.expense
+			};
+		},
+		// ----------------
+
+		// ----------------
+		testing         : function() {
 			console.log(data);
 		}
+		// ----------------
 	};
 })();
 // ******************************************************************************************
+// ******************************************************************************************
 
 // UI Controller ****************************************************************************
+// ******************************************************************************************
 var UIController = (function() {
 	var DOMStrings = {
-		inputTypeIncome  : document.getElementById('income-button').id,
-		inputTypeExpense : document.getElementById('expense-button').id,
-		inputDescription : document.getElementById('input-description').className,
-		inputValue       : document.getElementById('input-value').className,
-		inputButton      : document.getElementById('submit-button').id,
-		incomeContainer  : '.income-box__items',
-		expenseContainer : '.expense-box__items'
+		inputTypeIncome   : document.getElementById('income-button').id,
+		inputTypeExpense  : document.getElementById('expense-button').id,
+		inputDescription  : document.getElementById('input-description').className,
+		inputValue        : document.getElementById('input-value').className,
+		inputButton       : document.getElementById('submit-button').id,
+		incomeContainer   : '.income-box__items',
+		expenseContainer  : '.expense-box__items',
+		totalBudgetLabel  : '.output-box__display__screen-value',
+		totalIncomeLabel  : '.output-box__display__total--income-value',
+		totalExpenseLabel : '.output-box__display__total--expense-value',
+		currentDate       : '.output-box__display__date'
 	};
 
-	console.log(DOMStrings.inputDescription, DOMStrings.inputValue);
+	document.querySelector(DOMStrings.currentDate).textContent = new Date().toLocaleDateString();
 
 	return {
+		// ----------------
 		getInput      : function() {
 			return {
 				type        : (function() {
@@ -81,10 +132,12 @@ var UIController = (function() {
 					}
 				})(),
 				description : document.querySelector('.' + DOMStrings.inputDescription).value,
-				value       : document.querySelector('.' + DOMStrings.inputValue).value
+				value       : parseFloat(document.querySelector('.' + DOMStrings.inputValue).value)
 			};
 		},
+		// ----------------
 
+		// ----------------
 		addListItem   : function(obj, type) {
 			var html, newHtml, element;
 
@@ -108,7 +161,9 @@ var UIController = (function() {
 			// Insert the HTML into DOM
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 		},
+		// ----------------
 
+		// ----------------
 		clearFields   : function() {
 			var fields, fieldsArr;
 			fields = document.querySelectorAll('.' + DOMStrings.inputDescription + ', ' + '.' + DOMStrings.inputValue);
@@ -122,54 +177,92 @@ var UIController = (function() {
 
 			fieldsArr[0].focus();
 		},
+		// ----------------
 
+		// ----------------
+		displayBudget : function(obj) {
+			document.querySelector(DOMStrings.totalBudgetLabel).textContent = obj.budget;
+			document.querySelector(DOMStrings.totalIncomeLabel).textContent = obj.totalIncome;
+			document.querySelector(DOMStrings.totalExpenseLabel).textContent = obj.totalExpense;
+		},
+		// ----------------
+
+		// ----------------
 		getDOMStrings : function() {
 			return DOMStrings;
 		}
+		// ----------------
 	};
 })();
 // ******************************************************************************************
+// ******************************************************************************************
 
 // Global APP Controller ********************************************************************
+// ******************************************************************************************
 var controller = (function(budgetCtrl, UICtrl) {
+	// ----------------
 	var setupEventListeners = function() {
 		var DOM = UICtrl.getDOMStrings();
 
 		document.getElementById(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
 		document.addEventListener('keypress', function(event) {
 			if (event.keyCode === 13 || event.which === 13) {
 				ctrlAddItem();
 			}
 		});
 	};
+	// ----------------
 
+	// ----------------
+	var updateBudget = function() {
+		// 1. Calculate the budget
+		budgetCtrl.calculateBudget();
+
+		// 2. Return the budget
+		var budget = budgetCtrl.getBudget();
+
+		// 3. Displplay the buget on UI
+		UICtrl.displayBudget(budget);
+	};
+	// ----------------
+
+	// ----------------
 	var ctrlAddItem = function() {
 		var input, newItem;
 
 		//  1. Get the feild input data
 		input = UICtrl.getInput();
 
-		// 2. Add the item to Budget controller
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+			// 2. Add the item to Budget controller
+			newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-		// 3. Add the item to UI controller
-		UICtrl.addListItem(newItem, input.type);
+			// 3. Add the item to UI controller
+			UICtrl.addListItem(newItem, input.type);
 
-		// 4. Clear the fields.
-		UICtrl.clearFields();
+			// 4. Clear the fields.
+			UICtrl.clearFields();
 
-		// 5. Calculate the budget
-		// 6. Displplay the buget on UI
+			// 5. Calculate and  update budget
+			updateBudget();
+		}
 	};
+	// ----------------
 
 	return {
 		init : function() {
 			console.log('App started');
+
+			UICtrl.displayBudget({
+				budget       : 0,
+				totalIncome  : 0,
+				totalExpense : 0
+			});
 			setupEventListeners();
 		}
 	};
 })(budgetController, UIController);
+// ******************************************************************************************
 // ******************************************************************************************
 
 controller.init();
